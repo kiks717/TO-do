b076d013d5ce6f760a4107a677f9930b27993ac5
kiks717 <96001243+kiks717@users.noreply.github.com>
2022-01-25 13:08:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+let toDo = []
+
+const clearBtn = document.getElementById("clear")
+let ulEl = document.getElementById('list')
+const inputEL = document.getElementById("input-el")
+const leadsToDo = JSON.parse(localStorage.getItem("toDo"))
+const submitBtn = document.getElementById("submit")
+const xBtn = document.getElementById("x")
+const toDoLead = JSON.parse(localStorage.getItem("toDO"))
+console.log(toDoLead)
+
+
+if (leadsToDo){
+    toDo = leadsToDo
+    render(toDo)
+}
+   
+
+function render(list){
+        let listItem = " "
+    for(let i = 0; i<list.length;i++){
+        listItem+= `<li id='items'>
+         ${list[i]}  &nbsp &nbsp <button id='x'>x</button>
+        </li>`
+    }
+    
+    ulEl.innerHTML = listItem
+}
+
+
+submitBtn.addEventListener("click", function(){
+    toDo.push(inputEL.value)
+    inputEL.value = ""
+    localStorage.setItem("toDo", JSON.stringify(toDo))
+    render(toDo)
+})
+
+clearBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    toDo = []
+    render(toDo)
+})
+
+
+
+
+   
